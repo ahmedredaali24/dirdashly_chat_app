a54506392406360c4205771538b2e0eb1aa80975
ahmedreda24 <masry5357@gmail.com>
2024-10-24 14:18:20 +0300

    added splash screen and icon launcher

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,17 @@
+import 'package:bloc/bloc.dart';
 import 'package:dirdashly/ui/home/home_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'firebase_options.dart';
+import 'my_bloc_observe.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  Bloc.observer = MyBlocObserver();
   runApp(const MyApp());
 }
 
@@ -22,7 +25,9 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'chat App',
       initialRoute: HomeScreen.routeName,
-      routes: {HomeScreen.routeName: (context) => HomeScreen()},
+      routes: {
+        HomeScreen.routeName: (context) => HomeScreen(),
+      },
     );
   }
 }
